83b0bd514d4200bcee26789ed1709688a96b3f70
Edson N. Ferrao <nicferrao@gmail.com>
2026-02-14 13:34:04 -0300

    feat(particles): implement pooling in spawn system

--- Assets/ElementLogicFail/Scripts/Systems/Particles/ParticleSystem.cs
+++ Assets/ElementLogicFail/Scripts/Systems/Particles/ParticleSystem.cs
@@ -1,6 +1,8 @@
 using ElementLogicFail.Scripts.Components.Particles;
+using ElementLogicFail.Scripts.Components.Pool;
 using ElementLogicFail.Scripts.Components.Request;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Transforms;
 
@@ -23,21 +25,65 @@ namespace ElementLogicFail.Scripts.Systems.Particles
                 SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
             EntityCommandBuffer entityCommandBuffer = entitySimulationCommandBufferSystem.CreateCommandBuffer(state.WorldUnmanaged);
 
+            NativeParallelHashMap<Entity, Entity> prefabToPoolMap = new NativeParallelHashMap<Entity, Entity>(16, Allocator.TempJob);
+
+            foreach (var (pool, entity) in SystemAPI.Query<RefRO<ParticlePool>>().WithEntityAccess())
+            {
+                if (pool.ValueRO.Prefab != Entity.Null)
+                {
+                    prefabToPoolMap.TryAdd(pool.ValueRO.Prefab, entity);
+                }
+            }
+
             foreach (var (request, entity) in SystemAPI
                          .Query<DynamicBuffer<ParticleSpawnRequest>>().WithEntityAccess())
             {
                 foreach (var spawnRequest in request)
                 {
-                    var instance = entityCommandBuffer.Instantiate(spawnRequest.Prefab);
-                    entityCommandBuffer.SetComponent(instance, LocalTransform.FromPosition(spawnRequest.Position));
-                    entityCommandBuffer.AddComponent(instance, new ParticleEffectData
+                    Entity instance;
+                    if (prefabToPoolMap.TryGetValue(spawnRequest.Prefab, out Entity poolEntity))
                     {
-                        Lifetime = 1f,
-                        Timer = 0f
-                    });
+                        var poolBuffer = SystemAPI.GetBuffer<PooledEntity>(poolEntity);
+                        if (poolBuffer.Length > 0)
+                        {
+                            instance = poolBuffer[poolBuffer.Length - 1].Value;
+                            poolBuffer.RemoveAt(poolBuffer.Length - 1);
+
+                            entityCommandBuffer.RemoveComponent<Disabled>(instance);
+                            entityCommandBuffer.SetComponent(instance, LocalTransform.FromPosition(spawnRequest.Position));
+                            entityCommandBuffer.SetComponent(instance, new ParticleEffectData
+                            {
+                                Lifetime = 1f,
+                                Timer = 0f
+                            });
+                        }
+                        else
+                        {
+                            instance = entityCommandBuffer.Instantiate(spawnRequest.Prefab);
+                            entityCommandBuffer.AddComponent(instance, new ParentPool { PoolEntity = poolEntity });
+                            entityCommandBuffer.SetComponent(instance, LocalTransform.FromPosition(spawnRequest.Position));
+                            entityCommandBuffer.AddComponent(instance, new ParticleEffectData
+                            {
+                                Lifetime = 1f,
+                                Timer = 0f
+                            });
+                        }
+                    }
+                    else
+                    {
+                        instance = entityCommandBuffer.Instantiate(spawnRequest.Prefab);
+                        entityCommandBuffer.SetComponent(instance, LocalTransform.FromPosition(spawnRequest.Position));
+                        entityCommandBuffer.AddComponent(instance, new ParticleEffectData
+                        {
+                            Lifetime = 1f,
+                            Timer = 0f
+                        });
+                    }
                 }
                 request.Clear();
             }
+            
+            state.Dependency = prefabToPoolMap.Dispose(state.Dependency);
         }
 
         [BurstCompile]
